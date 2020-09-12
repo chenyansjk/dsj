bd3ace38339b8c1adff16e51fc657beb8b018397
chenyansjk <2546229043@qq.com>
2020-09-12 09:20:52 +0800

    退出功能

--- asster/js/haseApi.js
+++ asster/js/haseApi.js
@@ -1,3 +1,8 @@
 $.ajaxPrefilter(function (option) {
   option.url = 'http://ajax.frontend.itheima.net' + option.url
+  if (option.url.indexOf('/my/') !== -1) {
+    option.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
 })
---
+++ asster/js/index.js
@@ -0,0 +1,38 @@
+$(function () {
+  var layer = layui.layer
+  getUserInfo()
+  function getUserInfo() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/userinfo',
+    
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        renderAvatar(res.data)
+
+      }
+    })
+  }
+  function renderAvatar(user) {
+var name = user.nicknme || user.username
+$('.welcome').html('欢迎&nbsp;&nbsp' + name)
+if (user.user_pic !== null) {
+  $('.layui-nav-img').attr('src',user.user_pic).show()
+  $('.text-avatar').hide()
+} else {
+  var first = name[0].toUpperCase()
+  $('.text-avatar').html(first).show()
+  $('.layui-nav-img').hide()
+}
+  }
+  $('#logout').on('click', function () {
+    layer.confirm('确定退出登录？',{ icon: 3, title: '提示' }, function (index) {
+      localStorage.removeItem('token')
+      location.href= '/login.html'
+      layer.close(index)
+    })
+ })
+ 
+})
